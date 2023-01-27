1806d7d2226505692743b8a0350f6b455bb599b4
LeeSeungYeon <masakisqq@naver.com>
2023-01-27 12:00:45 +0900

    함수 - 화살표 선언 방식

---
+++ js/08func.js
@@ -0,0 +1,59 @@
+function computeLeapYear() {
+    let result = `평년입니다`;
+
+    if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+        return `윤년입니다`;
+    }
+
+    return `${year}년은 ${result}`;
+}
+
+function showStarPyramid(floor) {
+    result = ``;
+
+    for (let j = 1; j <= floor; ++j) {
+        for (let i = 1; i <= j; ++i) {
+            result += `*`;
+        }
+        result += `<br>`;
+    }
+    document.write(result);
+}
+
+function showAMPM(time) {
+    let hour = time[0] + time[1];
+    let result = `오후입니다`;
+
+    if (hour < 12) {
+        result = `오전입니다`;
+    }
+
+    return `${hour}시는 ${result}`;
+}
+
+function computeSungJuk(kor, eng, mat) {
+    let tot = 0, avg = 0, grd = `가`;  // 다중변수 선언
+
+    tot = kor + eng + mat;
+    avg = Math.floor(tot / 3);
+    switch (parseInt(avg / 10)) {
+        case 10:
+        case 9:
+            grd = `수`;
+            break;
+        case 8:
+            grd = `우`;
+            break;
+        case 7:
+            grd = `미`;
+            break;
+        case 6:
+            grd = `양`;
+            break;
+        default:
+            grd = `가`;
+            break;
+    }
+
+    document.write(`총점은 ${tot}, 평균은${avg}, 학점은${grd}입니다 <br>`);
+}
